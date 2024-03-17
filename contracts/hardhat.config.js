9902c6c4f8ee720550564c0f9d5e75da4f97672d
Nsima George <georgensima@gmail.com>
2024-03-17 01:54:51 +0100

    Updated commit

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  networks: { 
+    sepolia: {
+       url: `https://eth-sepolia.g.alchemy.com/v2/FthhIZLt21hwLO-GlNdTeKCS3Jbx_YsW`,
+       accounts: [process.env.PRIVATE_KEY],
+     },
+  },
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
 };
