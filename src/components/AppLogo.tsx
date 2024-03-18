c39eac76d3e8ef92b7489b028dde9f4a7b05c00a
Nsima George <georgensima@gmail.com>
2024-03-18 11:23:14 +0100

    Initial Commit

--- src/components/AppLogo.tsx
+++
@@ -1,5 +0,0 @@
-type Props = {title: string}
-
-export default function AppLogo({title}:Props) {
-  return <h2 className="tracking-wide text-lg font-semibold"> {title} </h2>
-}
--- src/components/Container.tsx
+++
@@ -1,9 +0,0 @@
-import clsx from "clsx";
-import { PropsWithChildren } from "react";
-
-type Props = PropsWithChildren & { className?: string }
-export default function Container({ children, className }: Props) {
-  return <div className={clsx("px-6 my-4", className)}>
-    {children}
-  </div>
-}
